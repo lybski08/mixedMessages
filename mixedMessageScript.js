be7443a0c2c0fcd262d52142d1b33bf4931f9585
lybski08 <78312803+lybski08@users.noreply.github.com>
2021-02-09 17:06:21 +0000

    Add files via upload

--- mixedMessageScript.js
+++ mixedMessageScript.js
@@ -1,5 +1,12 @@
-const inspiringSentence = ['I am beautiful'];
-console.log(inspiringSentence);
+const myHoroscope ={
+    love: ['Be aware to new people you may meet.','The heart wants what the heart wants. No need to fight it','Be cautious, your current relationship is fragile. Try to preserve it better.'],
+    money: ['Uh uh! If you have it, you can spend it. You are not taking it to the grave either way.','The fortune that you have is at risk. Be cautious in your investments.', 'Be patient. There is a good investment nearby. It could be life changer.'],
+    health: ['You have a strong heart. Keep active. Nothing can break you down.', 'You kinda have to be cautious. You are not getting any younger.', 'DANGER! Your immune system is very low. Be extra carefull']
+};
+console.log(myHoroscope.love + myHoroscope.money + myHoroscope.health);
+
+
+
 
 
 
